ad5c02f4eb02d6a4789a97b639bc30a529344b71
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-07-01 19:32:30 +0000

    primera version

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,37 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+const Containr = (props) => {
+    return (
+		<div className="container col-6 mt-2">
+			<nav className="navbar navbar-dark bg-dark">
+				<div id="timer" className="container d-flex text-white px-5">
+					<div className="rounded-4" id="clock"><i class="far fa-clock"></i></div>
+                    <div className="rounded-4">{props.digit6}</div>
+                    <div className="rounded-4">{props.digit5}</div>
+                    <div className="rounded-4">{props.digit4}</div>
+                    <div className="rounded-4">{props.digit3}</div>
+                    <div className="rounded-4">{props.digit2}</div>
+                    <div className="rounded-4">{props.digit1}</div>
+				</div>
+			</nav>
+		</div>
+	);
+};
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+setInterval(function(){
+    const sixthPlace = Math.floor((counter/100000)%10);
+    const fifthPlace = Math.floor((counter/10000)%10);
+    const fourthPlace = Math.floor((counter/1000)%10);
+    const thirdPlace = Math.floor((counter/100)%10);
+    const secondPlace = Math.floor((counter/10)%10);
+    const firstPlace = Math.floor((counter)%10);
+    console.log(sixthPlace,fifthPlace,fourthPlace,thirdPlace,secondPlace,firstPlace);
+    counter++;
+    ReactDOM.render(<Containr digit6={sixthPlace} digit5={fifthPlace} digit4={fourthPlace} digit3={thirdPlace} digit2={secondPlace} digit1={firstPlace} />, document.querySelector("#app"));
+},1000)
+
+
